a84e79746d58038c3277bedd1cc83fcec7533a4b
Yaroslav Mikhalov <badalabama22@gmail.com>
2023-12-05 17:23:14 +0200

    done

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,25 @@ import './style.css';
 
 const app = document.getElementById('app') as HTMLElement;
 let isActive = false;
+let btnIsClicked = false;
 
 const timeOutDuration: number = 1000;
-let count: number = 20;
+
 const countMessage: HTMLHeadingElement = document.createElement('h1');
 
 //! FUNCTIONS
 const timer: Function = (count: number, message: HTMLElement) => {
-  setInterval(() => {
-    if (count !== -1) {
+  const intervalId = setInterval(() => {
+    if (count >= 0) {
       message.textContent = `${count}`;
       count--;
+    } else {
+      clearInterval(intervalId);
+      location.reload();
     }
   }, 1000);
 };
+
 const leaveBtnEvent: Function = (
   leaveBtn: HTMLButtonElement,
   modalWindow: HTMLElement
@@ -27,32 +32,38 @@ const leaveBtnEvent: Function = (
     countMessage.className = 'leaving__timer';
     createMessage('Goodbye!', modalWindow);
     timer(count, countMessage);
-    setTimeout(() => {
-      location.reload();
-    }, 5000);
     modalWindow.append(countMessage);
     modalTimer?.remove();
-
     leaveBtn.remove();
   });
 };
+
 const createMessage: Function = (message: string, modalWindow: HTMLElement) => {
   const goodbyeMessage = document.createElement('h1');
   goodbyeMessage.textContent = message;
   modalWindow.append(goodbyeMessage);
 };
+
 function createModalWindow() {
   const modalWindow = document.createElement('div') as HTMLElement;
   modalWindow.className = 'modal__window';
   createButtons(modalWindow);
-  setInterval(() => {
+
+  let count: number = 20;
+  const countdownInterval = setInterval(() => {
     if (!isActive) {
       timer(count, countMessage);
       modalWindow.append(countMessage);
       app.append(modalWindow);
+      isActive = true;
+    } else if (isActive && btnIsClicked) {
+      clearInterval(countdownInterval);
+      count = 20;
+      isActive = false;
+      btnIsClicked = false;
+      countMessage.remove();
     }
-    isActive = false;
-  }, 5000);
+  }, 20000);
 }
 
 function createButtons(modalWindow: HTMLElement) {
@@ -60,13 +71,15 @@ function createButtons(modalWindow: HTMLElement) {
   stayBtn.className = 'stayBtn';
   stayBtn.textContent = 'stay';
   stayBtn.addEventListener('click', () => {
-    modalWindow.remove();
-    isActive = true;
+    btnIsClicked = true;
   });
+
   const leaveBtn = document.createElement('button') as HTMLButtonElement;
   leaveBtn.className = 'leaveBtn';
   leaveBtn.textContent = 'leave';
   leaveBtnEvent(leaveBtn, modalWindow);
+
   modalWindow.append(stayBtn, leaveBtn);
 }
+
 setTimeout(createModalWindow, timeOutDuration);
